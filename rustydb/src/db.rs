2c7d79d003407ebc30bbed237e5910ad0d58e7e2
Mohit Sharma <mohits1005.dev@gmail.com>
2026-03-25 16:15:23 +0530

    compaction

--- rustydb/src/db.rs
+++ rustydb/src/db.rs
@@ -107,5 +107,59 @@ impl Database {
         self.store.clear();
 
         println!("Flushed to SSTable!");
+
+        if self.sst_count >= 3 {
+            self.compact();
+        }
+    }
+
+    fn compact(&mut self) {
+        println!("Running compaction...");
+
+        let mut merged: HashMap<String, String> = HashMap::new();
+
+        // Read from oldest → newest
+        for i in 1..=self.sst_count {
+            let filename = format!("data/sst_{}.txt", i);
+
+            if let Ok(file) = File::open(&filename) {
+                let reader = BufReader::new(file);
+
+                for line in reader.lines() {
+                    let line = line.unwrap();
+                    let parts: Vec<&str> = line.split_whitespace().collect();
+
+                    if parts.len() == 2 {
+                        merged.insert(parts[0].to_string(), parts[1].to_string());
+                    }
+                }
+            }
+        }
+
+        // Convert to sorted vector
+        let mut entries: Vec<_> = merged.into_iter().collect();
+        entries.sort_by_key(|(k, _)| k.clone());
+
+        // Delete old SST files
+        for i in 1..=self.sst_count {
+            let filename = format!("data/sst_{}.txt", i);
+            let _ = std::fs::remove_file(filename);
+        }
+
+        // Write new compacted SST
+        let filename = "data/sst_1.txt";
+        let mut file = File::create(filename).expect("Failed to create compacted SST");
+
+        for (key, value) in entries {
+            let line = format!("{} {}\n", key, value);
+            file.write_all(line.as_bytes()).unwrap();
+        }
+
+        file.flush().unwrap();
+
+        // Reset state
+        self.sst_count = 1;
+
+        println!("Compaction complete!");
     }
 }
